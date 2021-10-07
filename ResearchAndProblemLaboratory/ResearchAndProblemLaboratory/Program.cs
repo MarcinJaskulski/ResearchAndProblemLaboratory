a327de6105c4ac822c0be086aa9ceffebeca128e
MarcinJaskulski <mrcn.jaskulski@gmail.com>
2021-10-07 16:20:48 +0200

    -utworzenie GetErlangDistribution

---
+++ ResearchAndProblemLaboratory/ResearchAndProblemLaboratory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ResearchAndProblemLaboratory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = DataGenerator.GetErlangDistribution(10, 1, 5);
+        }
+    }
+}
